d0a9a5e776a72927bf5a0307791a7c8edb1ab7fd
prasana-ramesh <prasana@fastmail.com>
2016-06-15 08:20:21 -0500

    removing routes not needed for authorization

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,6 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 
 // Load models
 const User = require('./models/user');
-const Chat = require('./models/chat');
 
 // Export the routes for our app to use
 module.exports = function(app) {
@@ -62,73 +61,12 @@ module.exports = function(app) {
             const token = jwt.sign(user, config.secret, {
               expiresIn: 10080 // in seconds
             });
-            res.status(200).json({ success: true, token: 'JWT ' + token });
+            res.status(200).json({ success: true, token: 'JWT ' + token, email: user.email });
           } else {
             res.status(401).json({ success: false, message: 'Authentication failed. Passwords did not match.' });
           }
         });
       }
-    });
-  });
-
-  // Protect chat routes with JWT
-  // GET messages for authenticated user
-  apiRoutes.get('/chat', requireAuth, function(req, res) {
-    Chat.find({$or : [{'to': req.user._id}, {'from': req.user._id}]}, function(err, messages) {
-      if (err)
-        res.status(400).send(err);
-
-      res.status(400).json(messages);
-    });
-  });
-
-  // POST to create a new message from the authenticated user
-  apiRoutes.post('/chat', requireAuth, function(req, res) {
-    const chat = new Chat();
-        chat.from = req.user._id;
-        chat.to = req.body.to;
-        chat.message_body = req.body.message_body;
-
-        // Save the chat message if there are no errors
-        chat.save(function(err) {
-            if (err)
-                res.status(400).send(err);
-
-            res.status(201).json({ message: 'Message sent!' });
-        });
-  });
-
-  // PUT to update a message the authenticated user sent
-  apiRoutes.put('/chat/:message_id', requireAuth, function(req, res) {
-    Chat.findOne({$and : [{'_id': req.params.message_id}, {'from': req.user._id}]}, function(err, message) {
-      if (err)
-        res.send(err);
-
-      message.message_body = req.body.message_body;
-
-      // Save the updates to the message
-      message.save(function(err) {
-        if (err)
-          res.send(err);
-
-        res.json({ message: 'Message edited!' });
-      });
-    });
-  });
-  
-  //GET Test for Authenticated user
-  apiRoutes.get('/getUserId', requireAuth, function(req, res) {
-	res.json({ userId: req.user._id });
-  });  
-
-  
-  // DELETE a message
-  apiRoutes.delete('/chat/:message_id', requireAuth, function(req, res) {
-    Chat.findOneAndRemove({$and : [{'_id': req.params.message_id}, {'from': req.user._id}]}, function(err) {
-      if (err)
-        res.send(err);
-
-      res.json({ message: 'Message removed!' });
     });
   });
 
