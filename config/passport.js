6808316a6cb5025dde026b2e4244c219ccd9ba45
Prasana Ramesh <prasana@fastmail.com>
2016-06-18 21:56:35 -0500

    adding passport.js config

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../app/models/user');
+const config = require('../config/main');
+
+// Setup work and export for the JWT passport strategy
+module.exports = function(passport) {
+  const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeader(),
+    secretOrKey: config.secret
+  };
+  passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    console.log("called here inside jwt strategy")
+    User.findOne({id: jwt_payload.id}, function(err, user) {
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        done(null, user);
+      } else {
+        done(null, false);
+      }
+    });
+  }));
+};
